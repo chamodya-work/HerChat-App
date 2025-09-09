38ea958780a5c5026303524fa98921001072cd62
chamodya <chamodyarajapaksha1@gmail.com>
2025-09-10 01:39:25 +0530

    added controllers

---
+++ backend/src/controllers/auth.controllers.js
@@ -0,0 +1,11 @@
+export async function signup(req,res){
+    res.send("Signup routed");
+}
+
+export async function login(req,res){
+    res.send("login routed");
+}
+
+export function  logout(req,res){
+    res.send("logout routed");
+}

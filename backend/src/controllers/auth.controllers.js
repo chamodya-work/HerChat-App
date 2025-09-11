aa968b3d6a5a231b5e3e6ed7af4f73e9836a11af
chamodya <chamodyarajapaksha1@gmail.com>
2025-09-11 14:43:35 +0530

    completed signup method

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 export async function signup(req, res) {
     const { fullName, email, password } = req.body; //destructuring req.body
@@ -23,10 +24,39 @@ export async function signup(req, res) {
         }
 
         const idx=Math.floor(Math.random()*100)+1; //1-100 numbers for profile pic selection
+        const randomAvatar=`https://avatar.iran.liara.run/public/${idx}.png`;
         
+        const newUser=new User(
+            {
+                email,
+                password,
+                fullName,
+                profilePic:randomAvatar
+            }
+        );
+
+        // TODO; CREATE THE USER IN STREAM AS WEll 
+
+        const token=jwt.sign(
+            {userId:newUser._id},
+            process.env.JWT_SECRET_KEY,
+            {expiresIn:'7d'}
+        );
+
+        res.cookie('jwt',token,{
+            maxAge:7*24*60*60*1000, //7 days in milliseconds
+            httpOnly:true, //accessible only by web server
+            sameSite:"strict",//CSRF
+            secure:process.env.NODE_ENV==='production' //https only in production
+        });
+
+        res.status(201).json({success:true,user:newUser});
+
+
             
     } catch(err){
-
+        console.log("Error in signup Controller",err);
+        res.status(500).json({message: "Internal Server Error"});
     }
 
 }

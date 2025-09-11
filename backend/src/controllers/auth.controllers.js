7b6d279662f1d3c4173ea304b7a1f7a6d3e8d80f
chamodya <chamodyarajapaksha1@gmail.com>
2025-09-11 23:17:17 +0530

    created method matchPassword in userSchema methods

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -65,7 +65,21 @@ export async function signup(req, res) {
 }
 
 export async function login(req, res) {
-  res.send("login routed");
+    try{
+        const {email,password}=req.body;
+        if(!email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+
+        const user=await User.findOne({email});
+        if(!user) return res.status(401).json({message:"Invalid email or password"});
+
+        const isPasswordCorrect=await user.matchPassword(password);
+
+    }
+    catch(err){
+
+    }
 }
 
 export function logout(req, res) {

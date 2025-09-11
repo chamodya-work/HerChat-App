0ad64a3629b13c93e9be4dda091d246d31c6c993
chamodya <chamodyarajapaksha1@gmail.com>
2025-09-11 15:04:51 +0530

    fix the type of constructor isuue using the await

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -26,7 +26,7 @@ export async function signup(req, res) {
         const idx=Math.floor(Math.random()*100)+1; //1-100 numbers for profile pic selection
         const randomAvatar=`https://avatar.iran.liara.run/public/${idx}.png`;
         
-        const newUser=new User(
+        const newUser=await User.create(
             {
                 email,
                 password,

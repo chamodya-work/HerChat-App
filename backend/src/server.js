5393d3ab6ebb071dad2eb7194ce2c3ca44b6facc
chamodya <chamodyarajapaksha1@gmail.com>
2025-09-11 10:58:30 +0530

    setup mongodb database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/db.js';
 
 dotenv.config();
 
@@ -13,4 +14,5 @@ app.use("/api/auth",authRoutes); //main route for auth
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });

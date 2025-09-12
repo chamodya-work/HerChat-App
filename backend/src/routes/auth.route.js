cd56c2a45c526688840b3e37ad331ed86847cbce
chamodya <chamodyarajapaksha1@gmail.com>
2025-09-12 14:34:25 +0530

    tested the route end points

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -9,4 +9,9 @@ router.post("/login",login);
 router.post("/logout",logout);
 router.post("/onboarding",protectRoute,onboard); //when we hitting "/onboarding api,before going to onbord controller it will go to protectRoute middleware to verify the user is authenticated or not"
 
+//check if the user is logged in 
+router.get("/me",protectRoute,(req,res)=>{
+    res.status(200).json({success:true,user:req.user}); //req.user is coming from protectRoute middleware
+});
+
 export default router;

8603e083845bcd5c5b7e6c79d3b94fc761b448c1
chamodya <chamodyarajapaksha1@gmail.com>
2025-09-12 11:13:22 +0530

    created stream platform related backend logic

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,24 @@
+import {StreamChat} from 'stream-chat';
+import "dotenv/config";
+
+const apiKey=process.env.STREAM_API_KEY;
+const apiSecret=process.env.STREAM_API_SECRET;
+
+if(!apiKey || !apiSecret){
+    console.error("Stream API key or Secret is missing");
+}
+
+const streamClient=StreamChat.getInstance(apiKey,apiSecret);
+
+//create or update a user in stream
+export const upsertStreamUser=async(userData)=>{
+    try{
+        await streamClient.upsertUsers([userData]);
+        return userData;
+    }
+    catch(err){
+        console.error("Error in upserting Stream user",err);
+    }
+};
+//todo: do it later
+export const generateStreamToken=(userId)=>{}

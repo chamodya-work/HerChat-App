b2b3f877f730855819b77af528a7606e6ed54a17
chamodya <chamodyarajapaksha1@gmail.com>
2025-09-21 13:04:54 +0530

    added basic routes to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { Routes } from "react-router";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage";
+import NotificationPage from "./pages/NotificationPage";
+import CallPage from "./pages/CallPage";
+import ChatPage from "./pages/ChatPage";
+import OnboardingPage from "./pages/OnboardingPage";
 
 const App = () => {
   return (
     <div className="h-screen">
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notification" element={<NotificationPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+      </Routes>
     </div>
   );
 };

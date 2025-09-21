5cffbb2d40d0bee2fc46809c37a040b9254b4552
chamodya <chamodyarajapaksha1@gmail.com>
2025-09-21 12:16:30 +0530

    setting dasyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,8 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="" data-theme="cupcake">
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+    <div className="h-screen text-5xl">
+      <h1>hello my friends,i'am from ceb</h1>
     </div>
   );
 };

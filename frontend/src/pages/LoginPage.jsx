f24fad76db6f2bc2626a3f85d83e8f8f59215a7c
chamodya <chamodyarajapaksha1@gmail.com>
2025-10-09 16:01:08 +0530

    update handleLogin method

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import React, { useState } from "react";
+import { login } from "../lib/api";
 
 const LoginPage = () => {
+  const { loginDate, setLoginData } = useState({
+    email: "",
+    password: "",
+  });
+
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: loginMutation,
+    isPending,
+    error,
+  } = useMutation({
+    mutationFn: login,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault(); //this is for prevent refreshing
+    loginMutation(loginDate);
+  };
+
   return <div>LoginPage</div>;
 };
 

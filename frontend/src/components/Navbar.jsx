1352d45f00c3b0d4fc489444726c45ccf5c27f02
chamodya <chamodyarajapaksha1@gmail.com>
2025-10-12 17:49:59 +0530

    defined logout fn

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import useAuthUser from "../hooks/useAuthUser";
 import { useLocation } from "react-router";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { logout } from "../lib/api";
 
 const Navbar = () => {
   const { authUser } = useAuthUser();
   const location = useLocation();
+  const isChatPage = location.pathname?.startsWith("/chat");
 
-  con = useLogin();
+  const queryClient = useQueryClient();
+
+  const { mutate: logoutMutation } = useMutation({
+    mutationFn: logout,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  });
+
   return <div>Navbar eka wadad bn </div>;
 };
 

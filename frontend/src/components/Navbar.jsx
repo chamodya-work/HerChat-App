995180fe3165636bea01fa3e4dc12b6ee54b3ca0
chamodya <chamodyarajapaksha1@gmail.com>
2025-10-12 13:26:19 +0530

    navbar initiated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import useAuthUser from "../hooks/useAuthUser";
+import { useLocation } from "react-router";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { authUser } = useAuthUser();
+  const location = useLocation();
+
+  con = useLogin();
+  return <div>Navbar eka wadad bn </div>;
 };
 
 export default Navbar;

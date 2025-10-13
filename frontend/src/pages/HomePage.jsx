684884bb2335ba90fae4d4ede1aa295ac7e83d8b
chamodya <chamodyarajapaksha1@gmail.com>
2025-10-13 12:23:14 +0530

    set userEffect to setOutgoingRequestsIds

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import React, { useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
+import {
+  getRecommendedUsers,
+  getUserFriends,
+  sendFriendRequest,
+} from "../lib/api";
 
 const HomePage = () => {
   const queryClient = useQueryClient(); //this is for invalidate some queries
 
-  const [outgoingRequestsIds, setOutgoingRequestsIds] = useState([]); // this is use to manage the friend requests button
+  const [outgoingRequestsIds, setOutgoingRequestsIds] = useState(new Set()); // this is use to manage the friend requests button
+  // Set use to store only unique values
 
   const { data: friends = [], isLoading: loadingFriends } = useQuery({
     queryKey: ["friends"],
@@ -15,6 +21,28 @@ const HomePage = () => {
     queryKey: ["users"],
     queryFn: getRecommendedUsers,
   });
+
+  const { data: outgoingFriendsReqs } = useQuery({
+    queryKey: ["outgoingFriendsReqs"],
+    queryFn: getOutgoingFriendsReqs,
+  });
+
+  const { mutate: sendRequestMutation, isPending } = useMutation({
+    mutationFn: sendFriendRequest,
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: ["outgoingFriendsReqs"] }),
+  });
+
+  //this is for the update the  setOutgoingRequestsIds
+  useEffect(() => {
+    const outgoingIds = new Set();
+    if (outgoingFriendsReqs && outgoingFriendsReqs.length > 0) {
+      outgoingFriendsReqs.forEach((req) => {
+        outgoingIds.add(req.recipient._id);
+      });
+      setOutgoingRequestsIds(outgoingIds);
+    }
+  }, [outgoingFriendsReqs]);
 
   return <div>HomePage</div>;
 };

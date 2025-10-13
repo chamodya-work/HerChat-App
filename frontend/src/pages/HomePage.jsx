19894ecc408d26ccf69f5d3ad8837940a56c08b2
chamodya <chamodyarajapaksha1@gmail.com>
2025-10-13 11:43:52 +0530

    added useQueries for the friends and recommended friends

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import React, { useState } from "react";
 
 const HomePage = () => {
+  const queryClient = useQueryClient(); //this is for invalidate some queries
+
+  const [outgoingRequestsIds, setOutgoingRequestsIds] = useState([]); // this is use to manage the friend requests button
+
+  const { data: friends = [], isLoading: loadingFriends } = useQuery({
+    queryKey: ["friends"],
+    queryFn: getUserFriends,
+  });
+
+  const { data: recommendedUsers = [], isLoading: loadingUsers } = useQuery({
+    queryKey: ["users"],
+    queryFn: getRecommendedUsers,
+  });
+
   return <div>HomePage</div>;
 };
 

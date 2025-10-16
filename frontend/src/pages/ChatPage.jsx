4b3513a9c9f7f60b061c7e6f58d5a8cd761b9d03
chamodya <chamodyarajapaksha1@gmail.com>
2025-10-16 15:37:58 +0530

    working with chatpage related fn

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,7 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { getAuthUser, getStreamToken } from "../lib/api";
 import { useQuery } from "@tanstack/react-query";
+
+import {
+  Channel,
+  ChannelHeader,
+  Chat,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
+import { StreamChat } from "stream-chat";
+import toast from "react-hot-toast";
+import ChatLoader from "../components/ChatLoader";
+
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;
 
 const ChatPage = () => {
   const { id } = useParams();
@@ -18,6 +33,49 @@ const ChatPage = () => {
     enabled: !!authUser, //now this query is only run when authUser is available
   });
 
+  useEffect(() => {
+    const initChat = async () => {
+      if (!tokenData.token || !authUser) return;
+
+      try {
+        console.log("initializing stram chat");
+        const client = StreamChat.getInstance(STREAM_API_KEY);
+
+        await client.connectUser(
+          {
+            id: authUser._id,
+            name: authUser.fullName,
+            image: authUser.profilePic,
+          },
+          tokenData.token
+        );
+
+        const channelId = [authUser._id, targetUserId].sort().join("-");
+
+        // you and me
+        // if i start the chat => channelId: [myId, yourId]
+        // if you start the chat => channelId: [yourId, myId]  => [myId,yourId]
+
+        const currChannel = client.channel("messaging", channelId, {
+          members: [authUser._id, targetUserId],
+        });
+
+        await currChannel.watch(); //this  is  working relatime in chat
+
+        setChatClient(client);
+        setChannel(currChannel);
+      } catch (error) {
+        console.log("Error initializing chat:", error);
+        toast.error("Could not connect to chat. Please try again.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    initChat();
+  }, []);
+
+  if (loading || !chatClient || !channel) return <ChatLoader />;
   return <div>ChatPage</div>;
 };
 

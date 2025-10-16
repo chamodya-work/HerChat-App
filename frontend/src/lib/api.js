9dd04f81b6eb13e5757223722d81c9ac52f42ed5
chamodya <chamodyarajapaksha1@gmail.com>
2025-10-16 14:06:08 +0530

    add getStreamToken method to work with Stream

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -61,4 +61,10 @@ export async function getFriendRequests() {
 export async function acceptFriendRequest(requestId) {
     const response = await axiosInstance.put(`/users/friend-request/${requestId}/accept`);
     return response.data;
-  }
+}
+
+export async function getStreamToken() {
+    const response = await axiosInstance.get("/chat/token");
+    return response.data;
+}
+

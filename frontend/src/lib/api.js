141feb6153cc5830d288eabbc7e6a979742a0c0a
chamodya <chamodyarajapaksha1@gmail.com>
2025-10-16 11:55:58 +0530

    added getFriendRequests to api

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -56,3 +56,9 @@ export async function getFriendRequests() {
     const response = await axiosInstance.get("/users/friend-requests");
     return response.data;
 }
+
+
+export async function acceptFriendRequest(requestId) {
+    const response = await axiosInstance.put(`/users/friend-request/${requestId}/accept`);
+    return response.data;
+  }

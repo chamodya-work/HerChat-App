78d6bfa5d4e9cd6161019abef2818fa46d632747
chamodya <chamodyarajapaksha1@gmail.com>
2025-10-16 14:42:15 +0530

    added useQuery for get stream token

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router";
+import { getAuthUser, getStreamToken } from "../lib/api";
+import { useQuery } from "@tanstack/react-query";
 
 const ChatPage = () => {
+  const { id } = useParams();
+
+  const [chatClient, setChatClient] = useState(null);
+  const [channel, setChannel] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const { authUser } = getAuthUser();
+
+  const { data: tokenData } = useQuery({
+    queryKey: ["streamToken"],
+    queryFn: getStreamToken,
+    enabled: !!authUser, //now this query is only run when authUser is available
+  });
+
   return <div>ChatPage</div>;
 };
 

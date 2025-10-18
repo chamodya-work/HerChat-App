5b0fb3532b6bc1fd68e091170ad6547356adeb8d
chamodya <chamodyarajapaksha1@gmail.com>
2025-10-18 11:33:47 +0530

    added route to call page

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -89,7 +89,17 @@ const ChatPage = () => {
     initChat();
   }, [tokenData, authUser, targetUserId]);
 
-  const handleVideoCall = () => {};
+  const handleVideoCall = () => {
+    if (channel) {
+      const callurl = `${window.location.origin}/call/${channel.id}`;
+
+      channel.sendMessage({
+        text: `I've started a video call. Join me here: ${callurl}`,
+      });
+
+      toast.success("Video call link sent successfully!");
+    }
+  };
 
   if (loading || !chatClient || !channel) return <ChatLoader />;
   return (
